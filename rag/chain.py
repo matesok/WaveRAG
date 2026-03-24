c7d471e2c7f15d424c2a7dc64fdd655e5802dcfc
mateusz <matsok1193@gmail.com>
2026-03-24 18:32:42 +0100

    WaveRAG

--- rag/chain.py
+++ rag/chain.py
@@ -7,11 +7,12 @@ from langchain_core.prompts import ChatPromptTemplate
 from rag.db import get_db
 
 OLLAMA_URL = os.getenv("OLLAMA_BASE_URL", "http://localhost:11434")
-llm = ChatOllama(model="llama3.2", base_url=OLLAMA_URL)
+llm = ChatOllama(model="llama3.2", base_url=OLLAMA_URL, num_predict=1024,)
 
 PROMPT_TEMPLATE = """
 You are a helpful assistant. Answer the question using ONLY the context below.
-If the answer is not in the context, say "I don't know".
+Always respond in the same language as the question.
+If the answer is not in the context, say "Nie wiem / I don't know".
 
 Context:
 {context}
@@ -23,11 +24,11 @@ Question: {question}
 def query_rag(question: str) -> Generator:
     t9 = time.time()
     db = get_db()
-    print(f"Time after start DB {time.time()-t9:.2f}s")
+    print(f"Time after start DB {time.time() - t9:.2f}s")
     t0 = time.time()
-    results = db.max_marginal_relevance_search(question,  k=2, fetch_k=8)
-    print(f"Retrieval (embedding + search): {time.time()-t0:.2f}s")
-    
+    results = db.max_marginal_relevance_search(question, k=4, fetch_k=12)
+    print(f"Retrieval (embedding + search): {time.time() - t0:.2f}s")
+
     print(f"QUESTION {question} ALL RESULTS:")
     for doc in results:
         print(f"Source: {doc.metadata['source']}")
@@ -41,15 +42,18 @@ def query_rag(question: str) -> Generator:
     context_text = "\n\n----\n\n".join([doc.page_content for doc in results])
     t1 = time.time()
     prompt = (ChatPromptTemplate
-        .from_template(PROMPT_TEMPLATE)
-        .format(context=context_text, question=question))
-    
+              .from_template(PROMPT_TEMPLATE)
+              .format(context=context_text, question=question))
+
     for chunk in llm.stream(prompt):
         yield chunk.content
-    print(f"LLM generation: {time.time()-t1:.2f}s")
+    print(f"LLM generation: {time.time() - t1:.2f}s")
+    seen = set()
     sources = "\n".join([
         f"- {os.path.basename(doc.metadata['source'])}"
         for doc in results
+        if not (os.path.basename(doc.metadata['source']) in seen
+                or seen.add(os.path.basename(doc.metadata['source'])))
     ])
 
     if sources:
